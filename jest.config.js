2c178e1572862c70f6c796085182d53c75b183ff
Max Milton <max@wearegenki.com>
2018-03-26 09:22:54 +1100

    Fix placeholder test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,20 @@
 'use strict'; // eslint-disable-line
 
 module.exports = {
-  preset: '@wearegenki/test',
+  // preset: '@wearegenki/test',
+  // globalSetup: '',
+  // globalTeardown: '',
+  // testEnvironment: '',
+  // setupTestFrameworkScriptFile: '',
+  // transform: {},
+
+  testPathIgnorePatterns: [
+    '/coverage/',
+    '/node_modules/',
+    '/\\.vscode/',
+    '/\\.cache/',
+    '/\\.git/',
+  ],
+  collectCoverageFrom: ['<rootDir>/index.js'],
+  coverageDirectory: '<rootDir>/test/coverage',
 };
--- test/placeholder.test.js
+++ test/placeholder.test.js
@@ -2,6 +2,8 @@
  * Placeholder until we're ready to write the real tests.
  */
 
-test('placeholder to pass', () => {
-  expect(2 + 3).toEqual(5);
+describe('Placeholder', () => {
+  it('should always pass', () => {
+    expect(2 + 3).toEqual(5);
+  });
 });
